63aa8d9748b63a50c9b998f2bc130d6537d441ae
Prasad Jayashanka <prasadjayashanka@gmail.com>
2015-12-23 01:20:41 +0530

    Deleted extra scene and cache files

--- Assets/scenes/splash/splashLinker.cs
+++ Assets/scenes/splash/splashLinker.cs
@@ -6,8 +6,8 @@ public class splashLinker : MonoBehaviour {
 	void Update () {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         { 
-                Debug.Log(Logger.Log("Loading Game Name Screen"));
-                Application.LoadLevel("GameName");
+                Debug.Log(Logger.Log("Loading Main Menu Screen"));
+                Application.LoadLevel("MainMenu");
         }
 	}
 }
---
+++ Assets/scenes/testlevel/testBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testBehaviour : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	transform.Rotate(Vector3.up * 1.2f);
+	}
+}
